3c227c3ee15f8df4dedbdc8d0442a9776255aefa
Aishwary Tandon <ashwarytandon@gmail.com>
2015-09-23 19:19:18 -0400

    A sample demo of Butter Knife Library

---
+++ app/src/main/java/com/example/aishwary/butterknifelibrary/SimpleApp.java
@@ -0,0 +1,13 @@
+package com.example.aishwary.butterknifelibrary;
+
+import android.app.Application;
+
+/**
+ * Created by Aishwary on 9/23/2015.
+ */
+public class SimpleApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
